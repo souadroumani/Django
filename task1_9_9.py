3924e3aca28b1eda0f3a170c5ca9370cecc8b014
Souad Roumani <82784548+souadroumani@users.noreply.github.com>
2025-09-09 09:15:14 +0300

    Add files via upload

---
+++ task1_9_9.py
@@ -0,0 +1,15 @@
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+
+number1 = int(input("Enter First Number : "))
+number2 = int(input("Enter Second Number : "))
+
+result_add = number1 + number2
+print(f"{number1} + {number2} = {result_add} ", type(result_add))
+result_sub = number1 - number2
+print(f"{number1} - {number2} = {result_sub} ", type(result_sub))
+result_mul = number1 * number2
+print(f"{number1} * {number2} = {result_mul} ", type(result_mul))
+result_div = number1 / number2
+print(f"{number1} / {number2} = {result_div} ", type(result_div))
+
